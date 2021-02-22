92598b66254bf5a39a4bf747bfa60f5b73c03c72
Pietro Piraino <curciopietro91@gmail.comgit config --global user.email curciopietro91@gmail.com>
2021-02-22 15:56:18 +0100

    second-commit

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const readline = require("readline");
 const consola = require("consola");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askForWords() {
+  rl.question("Inserisci una frase di 5 parole: ", (answer) => {
+    let wordCount = answer.match(/(\w+)/g).length;
+    console.log(wordCount);
+
+    if (wordCount === 5) {
+      consola.success("Ben fatto!");
+      rl.close();
+    } else {
+      consola.error(
+        `Mi dispiace, la tua frase contiene ${wordCount} parole. Ritenta!`
+      );
+      askForWords();
+    }
+    console.log(wordCount);
+  });
+}
+
+askForWords();
